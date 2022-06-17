fdf91c5266f1d1be0ca23a05bb18c44d3d63ebda
Cristian Silva <chrissilva9420@gmail.com>
2022-06-17 12:38:07 -0600

    Follow up commit.

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const User = mongoose.model('User')
+
+const router = express.Router();
+
+router.post('/signup',async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = new User({email,password})
+
+        await user.save();
+
+        const token = jwt.sign({userId: user._id}, 'MY_SECRET_KEY')
+
+        res.send({token:token})
+    }catch (e) {
+        return res.status(422).send(e)
+    }
+
+});
+
+router.post('/signin', async (req,res) => {
+    const { email, password } = req.body;
+
+    if(!email || !password){
+        return res.status(422).send({error: 'Must Provide email and password.'});
+    }
+
+    const user = await User.findOne({ email: email});
+
+    if(!user){
+        return res.status(422).send({error: 'Invalid password or email.'})
+    }
+
+    try{
+        await user.comparePassword(password);
+        const token = jwt.sign({userId: user._id},'MY_SECRET_KEY')
+        res.send({token})
+    }catch (e) {
+        return res.status(422).send({ error: 'Invalid password or email'})
+    }
+
+})
+
+module.exports = router;
